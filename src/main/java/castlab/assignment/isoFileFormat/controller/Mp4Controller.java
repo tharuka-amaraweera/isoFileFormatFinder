2df9ef01ab8e9d5f08834ea3092a376855c1ea13
tharuka-amaraweera <tharuka95amaraweera@gmail.com>
2023-11-17 09:04:02 +0100

    Added basic controller and service class

---
+++ src/main/java/castlab/assignment/isoFileFormat/controller/Mp4Controller.java
@@ -0,0 +1,21 @@
+package castlab.assignment.isoFileFormat.controller;
+
+import castlab.assignment.isoFileFormat.service.Mp4Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("mp4")
+public class Mp4Controller {
+
+    @Autowired
+    Mp4Service mp4Service;
+
+    @GetMapping("/analyze")
+    public String analyzeMp4File(@RequestParam String url){
+        return mp4Service.analyzeMp4(url);
+    }
+}
---
+++ src/main/java/castlab/assignment/isoFileFormat/service/Mp4Service.java
@@ -0,0 +1,10 @@
+package castlab.assignment.isoFileFormat.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Mp4Service {
+    public String analyzeMp4(String url) {
+        return url;
+    }
+}
