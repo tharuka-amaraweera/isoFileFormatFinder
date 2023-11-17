fcb07261d5f2e4d7baff53903422ac97b40432fb
tharuka-amaraweera <tharuka95amaraweera@gmail.com>
2023-11-17 08:34:33 +0100

    Initial commit

---
+++ src/main/java/castlab/assignment/isoFileFormat/IsoFileFormatApplication.java
@@ -0,0 +1,13 @@
+package castlab.assignment.isoFileFormat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoFileFormatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsoFileFormatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/castlab/assignment/isoFileFormat/IsoFileFormatApplicationTests.java
@@ -0,0 +1,13 @@
+package castlab.assignment.isoFileFormat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsoFileFormatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
