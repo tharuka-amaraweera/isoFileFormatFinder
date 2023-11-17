b41fb4b1b210787038c2c1188dcb60c7625818a9
tharuka-amaraweera <tharuka95amaraweera@gmail.com>
2023-11-17 09:38:53 +0100

    Added MP4 DTO and used InputStream class to read the bytes of the video

--- src/main/java/castlab/assignment/isoFileFormat/service/Mp4Service.java
+++ src/main/java/castlab/assignment/isoFileFormat/service/Mp4Service.java
@@ -1,10 +1,27 @@
 package castlab.assignment.isoFileFormat.service;
 
+import castlab.assignment.isoFileFormat.dto.Mp4Box;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
 
 @Service
 public class Mp4Service {
     public String analyzeMp4(String url) {
-        return url;
+        String byteArray=null;
+        try (InputStream inputStream = new URL(url).openStream()){
+            byte[] bytes = inputStream.readAllBytes();
+            byteArray = Arrays.toString(bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return byteArray;
+    }
+
+    private Mp4Box convertToBox (byte[] bytes, int offset){
+
     }
 }
